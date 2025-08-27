44ff7000c50cdee30c24b0e69b34e12e55882dd1
Jiro <jirogonzales777@gmail.com>
2025-08-27 12:03:11 +0800

    Added Database Connection

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -12,16 +12,13 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `ipt-keys_${name}`);
 
-export const posts = createTable(
-  "post",
-  (d) => ({
-    id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
-    createdAt: d
-      .timestamp({ withTimezone: true })
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
-  }),
-  (t) => [index("name_idx").on(t.name)],
-);
+export const apiKeys = createTable("api_keys", (d) => ({
+  id: d.text("id").primaryKey(),
+  name: d.varchar({ length: 256 }).notNull(),
+  createdAt: d
+    .timestamp({ withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: d.timestamp({ withTimezone: true }).$onUpdate(() => new Date()),
+  revoked: d.boolean("revoked").notNull().default(false),
+}));
